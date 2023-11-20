6bff06be2ae6a5bd716b410ea5353f890c4ffe8d
mariano <marianoppu@gmail.com>
2023-11-20 09:34:31 -0500

    Initial Commit

---
+++ FetchChallenge/FetchChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchChallengeApp.swift
+//  FetchChallenge
+//
+//  Created by Mariano Patino-Paul on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
